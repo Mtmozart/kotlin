2f524a7c64f7124194f54745b6b3f5b34dd9eeb3
matheus mozart <mmsnborges@gmail.com>
2024-05-21 13:48:37 -0400

    um grão

--- src/main/br/com/alugames/model/Gamer.kt
+++ src/main/br/com/alugames/model/Gamer.kt
@@ -1,5 +1,7 @@
 package br.com.alugames.model
 
+import kotlin.random.Random
+
 data class Gamer(var name: String, var email:String){
     var dateOfBirth: String = ""
     var user:String? = null;
@@ -8,10 +10,17 @@ data class Gamer(var name: String, var email:String){
     constructor(name: String, email: String, dateOfBirth:String, user:String): this(name, email){
         this.dateOfBirth = dateOfBirth
         this.user = user
+        createInternalId()
     }
 
     override fun toString(): String {
         return "Gamer(name='$name', email='$email', dateOfBirth='$dateOfBirth', user=$user, internalId=$internalId)"
+    }
+
+    fun createInternalId(){
+        val number = Random.nextInt(100000);
+        val tag = String.format("%04d", number);
+        internalId ="$user#$tag"
     }
 
 
