823b02bfa23a87f1bc5ca0ebe89c5e89bbb8947f
matheus mozart <mmsnborges@gmail.com>
2024-05-24 15:04:21 -0400

    lista e compation object

--- src/main/br/com/alugames/main/Main.kt
+++ src/main/br/com/alugames/main/Main.kt
@@ -1,43 +1,55 @@
 package br.com.alugames.main
 
+import br.com.alugames.model.Gamer
 import br.com.alugames.model.Jogo
 import br.com.alugames.services.Consumer
 import java.util.*
 
 fun main() {
+
     val read = Scanner(System.`in`)
-    println("Digite o código de jogo para busca:")
-    val busca = read.nextLine()
+    val gamer = Gamer.gamerCreate(read)
+    println("Cadastro concluído com sucesso. Dados: ");
+    println(gamer)
+    do {
+        println("Digite o código de jogo para busca:")
+        val busca = read.nextLine()
 
-    val findGameApi = Consumer()
-    var infoGame = findGameApi.search(busca);
+        val findGameApi = Consumer()
+        var infoGame = findGameApi.search(busca);
 
-    var myGame: Jogo? = null;
-    val result = runCatching {
-        myGame = Jogo(
-        infoGame.info.title,
-        infoGame.info.thumb
-        )
-    }
-    result.onFailure {
-        println("br.com.alugames.model.Jogo inexistente, tente outro id.");
-    }
-    result.onSuccess {
-        println("Desejas inserir uma descrição personalizada ? S/N")
-        val option = read.nextLine();
-        if (option.equals("s", ignoreCase = true)) {
-            println("Digite a descrição: ")
-            val description = read.nextLine();
-            myGame!!?.descricao = description;
-            println(myGame)
-        } else if (option.equals("n", ignoreCase = true)) {
-            myGame!!?.descricao = myGame?.titulo;
-            println(myGame)
-        } else {
-            println("Caractere inválido");
+        var myGame: Jogo? = null;
+        val result = runCatching {
+            myGame = Jogo(
+                infoGame.info.title,
+                infoGame.info.thumb
+            )
         }
-    }
-    result.onSuccess {
-        println("Busca finalizada com sucesso.")
-    }
+        result.onFailure {
+            println("br.com.alugames.model.Jogo inexistente, tente outro id.");
+        }
+        result.onSuccess {
+            println("Desejas inserir uma descrição personalizada ? S/N")
+            val option = read.nextLine();
+            if (option.equals("s", ignoreCase = true)) {
+                println("Digite a descrição: ")
+                val description = read.nextLine();
+                myGame!!?.descricao = description;
+
+            } else if (option.equals("n", ignoreCase = true)) {
+                myGame!!?.descricao = myGame?.titulo;
+
+            } else {
+                println("Caractere inválido");
+            }
+            gamer.findsGame.add(myGame)
+
+        }
+        println("Deseja buscar outro jogo? S/N")
+        val response = read.nextLine()
+    } while (response.equals("s", ignoreCase = true))
+
+    println("Lista de jogos buscados")
+    println(gamer.findsGame)
+    println("Busca finalizada com sucesso.")
 }
--- src/main/br/com/alugames/model/Gamer.kt
+++ src/main/br/com/alugames/model/Gamer.kt
@@ -1,27 +1,29 @@
 package br.com.alugames.model
 
+import java.util.Scanner
 import kotlin.random.Random
 
 data class Gamer(var name: String, var email: String) {
     var dateOfBirth: String = ""
     var user: String? = null
-        set(value){
+        set(value) {
             field = value
-            if (internalId.isNullOrBlank()){
+            if (internalId.isNullOrBlank()) {
                 createInternalId()
             }
         }
     var internalId: String? = null
         private set
-
+    val findsGame = mutableListOf<Jogo?>()
 
     constructor(name: String, email: String, dateOfBirth: String, user: String) : this(name, email) {
         this.dateOfBirth = dateOfBirth
         this.user = user
         createInternalId()
     }
+
     init {
-        if(name.isNullOrBlank()){
+        if (name.isNullOrBlank()) {
             throw IllegalArgumentException("Nome em branco.")
         }
         this.email = emailValidator()
@@ -41,11 +43,39 @@ data class Gamer(var name: String, var email: String) {
 
     fun emailValidator(): String {
         val regex = Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
-        if(regex.matches(email)){
+        if (regex.matches(email)) {
             return email;
-        } else{
+        } else {
             throw IllegalArgumentException("Email inválido.")
         }
 
+    }
+
+    companion object {
+        fun gamerCreate(read: Scanner): Gamer {
+            println("Boas vindas ao AluGames! Vamos fazer seu cadastro. Digite seu nome:")
+            val name = read.nextLine()
+            println("Digite seu e-mail:")
+            val email = read.nextLine()
+            println("Deseja completar seu cadastro com usuário e data de nascimento? (S/N)")
+            val option = read.nextLine()
+            if (option.equals("s", true)) {
+                println("Digite sua data de nascimento(DD/MM/AAAA):")
+                val dateOfBirth = read.nextLine()
+                println("Digite seu nome de usuário:")
+                val user = read.nextLine()
+
+                return Gamer(
+                    name,
+                    email,
+                    dateOfBirth,
+                    user
+                )
+            }
+            return Gamer(
+                name,
+                email
+            )
+        }
     }
 }
